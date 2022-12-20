838bc134b2136fd31119538ead4fdf24a9554757
annup76779 <annup76779@gmail.com>
2022-12-20 15:12:55 +0530

    Version 1 Completed

--- board.py
+++ board.py
@@ -1,5 +1,9 @@
+from functools import reduce
+
 from config import board_size, ships
 from ships import Ship
+
+from random import randint, choice
 
 
 class Board:
@@ -15,7 +19,8 @@ class Board:
             key = alpha[(col // 26) - 1] + alpha[col % 26]
         cols[key] = col
 
-    def __init__(self):
+    def __init__(self, player):
+        self.player = player
         self.board = [["."] * board_size[1] for _ in range(board_size[0])]
         self.onboard_ships: dict = {ship_name: Ship(ship_name, size) for ship_name, size in ships.items()}
 
@@ -30,7 +35,6 @@ class Board:
                 for index in range(size):
                     if self.board[y][index+x] != ".":
                         sum_of_free_place += 1
-            print(sum_of_free_place)
             return sum_of_free_place == 0
         except IndexError:
             return False
@@ -48,10 +52,11 @@ class Board:
         except IndexError:
             return
 
-    def update_board(self, x: str, y: int, direction: str, ship: str) -> bool:
+    def update_board(self, x: str, y: int, direction: str, ship: str, is_auto=False) -> bool:
         size: int = ships.get(ship)
         if size is None:
-            print("Failed: Ship %s not found" % ship, end="\n\n")
+            if not is_auto:
+                print("Failed: Boat %s not found" % ship, end="\n\n")
             return False
 
         if self.onboard_ships.get(ship).x is not None:
@@ -60,11 +65,13 @@ class Board:
         # get the col of the board
         col = self.cols.get(x.upper())
         if col is None:
-            print("Failed: Coordinate %s cannot be accessed." % x+str(y), end="\n\n")
+            if not is_auto:
+                print("Failed: Coordinate %s cannot be accessed." % x+str(y), end="\n\n")
             return False
 
         if not self.validate_ship_placement(col, y, direction, size):
-            print("Failed: to place %s at coordinates (%s, %d)" % (ship, x, y))
+            if not is_auto:
+                print("Failed: to place %s at coordinates (%s, %d)" % (ship, x, y))
             return False
 
         # Make a Ship class object to be used for the placement of the ship on the board
@@ -80,23 +87,61 @@ class Board:
                 self.board[index+y][col] = ship_obj
         elif direction.lower() == "h":  # for horizontal alignment of boat
             for index in range(size):
-                print("X", x, col)
                 self.board[y][index+col] = ship_obj
         return True
 
-    def hit(self, x:str, y:int):
+    def auto_place_remaining_ship(self):
+        for ship, ship_obj in self.onboard_ships.items():
+            if ship_obj.x is None:
+                y = randint(0, board_size[0])
+                x = choice(tuple(self.cols.keys()))
+                direction = choice(("H", "V"))
+                while not self.update_board(x, y, direction, ship, True):
+                    y = randint(0, board_size[0])
+                    x = choice(tuple(self.cols.keys()))
+                    direction = choice(("H", "V"))
+        return True
+
+    def hit(self, x:str, y:int, auto_hit=False):
         # get the col of the board
         col = self.cols.get(x.upper())
+        if self.board[y][col] == "M" and auto_hit:
+            return False
+        print("Coordinate", f"{x}{y}")
         coordinate_state = self.board[y][col]
         if isinstance(coordinate_state, Ship) and (not coordinate_state.is_sunken()):
             coordinate_state.hit()
-        self.board[y][col] = "H"
+            self.board[y][col] = "H"
+            print("%s%s is a 'Hit' on %s's boat!" % (x, y, self.player))
+        else:
+            self.board[y][col] = "M"
+            print("%s%d is a 'Miss' on %s's boat!" % (x, y, self.player))
+        return True
 
     def get_ship_sunken_status(self):
         return {key: value.is_sunken() for key, value in self.onboard_ships.items()}
 
+    @property
+    def board_status(self):
+        return all(boat_obj.is_sunken()==False for boat_obj in self.onboard_ships.values())
+
+    def get_placed_and_not_placed_ships(self) -> tuple:
+        """
+        Use this method to get the separated list of completed ships placement and not placed ships list
+        :return: tuple of placed and not placed ships
+        """
+        placed = []
+        not_placed = []
+        for ship_name, ship_obj in self.onboard_ships.items():
+            if ship_obj.x is not None:
+                placed.append(ship_name)
+            else:
+                not_placed.append(ship_name)
+        return placed, not_placed
+
     def __str__(self):
-        board = "  "+ (" ".join(self.cols.keys()) + "\n")
+        board = (str(self.player)+" Board").center(board_size[1] * 2)+"\n"
+        board += "+ " + (" ".join(self.cols.keys()) + "\n")
         row_index = 0
         for row in self.board:
             board += str(row_index)+" " + (" ".join(map(str, row)) + "\n")
